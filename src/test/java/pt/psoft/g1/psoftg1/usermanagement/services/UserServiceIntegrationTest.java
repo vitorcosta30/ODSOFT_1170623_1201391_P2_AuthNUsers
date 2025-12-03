369ebabdb40a5bac27c8a32a557ecafc4e475dfa
Vítor Costa <62137623+vitorcosta30@users.noreply.github.com>
2025-12-03 14:56:08 +0000

    Added empty IT to generate initial reports

---
+++ src/test/java/pt/psoft/g1/psoftg1/usermanagement/services/UserServiceIntegrationTest.java
@@ -0,0 +1,19 @@
+package pt.psoft.g1.psoftg1.usermanagement.services;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+class UserServiceIntegrationTest {
+
+}
